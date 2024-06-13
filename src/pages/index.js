18df4602e18c953b2a2c06192cecbe38e2010673
Apurvborhade <apurvaborhadee@gmail.com>
2024-06-13 19:40:09 +0530

    Landing Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
+import About from "@/components/About";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Landing from "@/components/Landing";
+import Works from "@/components/Works";
 
 
 export default function Home() {
   return (
-   <></>
+   <>
+    <Header />
+    <Landing />
+    <Works />
+    <About />
+    <Footer />
+   </>
   );
 }
