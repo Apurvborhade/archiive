0376202108cbfd8e3b43d14f89ee2a67bab3652c
Apurvborhade <apurvaborhadee@gmail.com>
2024-06-13 19:39:37 +0530

    Landing Page

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+import localFont from "@next/font/local"
+
+const neueMontreal = localFont({src:"../fonts/Neue Montreal Regular 400.otf"})
+const Header = () => {
+  return (
+    <div className={`header-container fixed w-full ${neueMontreal.className}`}>
+      <div className='header w-full flex justify-between items-center p-4 lg:px-10 lg:py-4'>
+        <div className="brand-logo">
+          <Image 
+            src="/assets/archiive.svg"
+            width={30}
+            height={30}
+            alt="Picture of the author"
+          />
+        </div>
+        <div className="header-cta flex items-center">
+          <button className='menu-btn flex mx-10'><p>Menu</p></button>
+          <button className='contact-cta border border-black rounded-md py-2 px-5 hover:bg-black hover:text-white transition-colors'>
+            <p>Book A Call</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Landing.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import localFont from '@next/font/local'
+
+const Helvetica = localFont({ src: "../fonts/helvetica/Helvetica-Bold.ttf" });
+const FreeSans = localFont({ src: "../fonts/FreeSansBold.ttf" });
+
+const Landing = () => {
+  return (
+
+    <div className="landing-container h-full flex flex-col place-content-between p-4 lg:p-10">
+      <div className="brand-info flex justify-end">
+        <h2 className={`${Helvetica.className} lg:w-3/6 text-4xl lg:text-5xl lg:mt-32 mt-44`}>We are a creative studio crafting unique visual content to empower people and brands.</h2>
+      </div>
+      <div className={`branding ${FreeSans.className}`}>
+        <div className="tagline-anim">
+          <h1 className=' text-8xl lg:text-9xl'>archiive</h1>
+          <div className="text-anim flex items-center text-lg lg:text-3xl">
+            <h2 className=''>We are here to archiive</h2>
+            <div className="dash w-24 bg-black border border-black mx-5"></div>
+            <div className='text-slide h-6 lg:h-10 w-auto lg:w-auto overflow-hidden'>
+              <div className='text-transform'>
+                <h2>Architecture</h2>
+                <h2>Landscape</h2>
+                <h2>Nature</h2>
+                <h2>Architecture</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Landing
