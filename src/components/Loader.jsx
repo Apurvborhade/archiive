3bd723de1d0abd75d6ba7e751846682d389aa027
Apurv <apurvabrhadee@gmail.com>
2024-06-20 09:33:18 +0530

    Page Loader + Horizontal Scroll

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,45 +1,94 @@
 import React from 'react'
 import gsap, { Power1 } from 'gsap'
 import { useGSAP } from '@gsap/react'
+import Image from 'next/image';
 gsap.registerPlugin(useGSAP);
+import localFont from '@next/font/local'
+const neueHass = localFont({ src: [
+  {
+    path:'../fonts/neue hass/NeueHaasDisplayRoman.ttf',
+    weight:'400',
+    style:'roman'
+  },
+  {
+    path:'../fonts/neue hass/NeueHaasDisplayMediu.ttf',
+    weight:'500',
+    style:'medium'
+  },
 
+] });
 const Loader = () => {
     useGSAP(() => {
-        gsap.from(
+
+        const tl = gsap.timeline({});
+
+        
+        tl.from('.loader-logo',{
+            x:200,
+            duration:1,
+            ease:"circ"
+        })
+
+        
+        
+        tl.fromTo('.loader-brand--text',{
+            clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)"
+        },{
+            duration:1,
+            clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+        },"-=0.4")
+
+        tl.to(".logo-anim",{opacity:0})
+        tl.from(
             '.block',
             {
                 width: "5%",
                 ease: Power1.easeIn,
-                duration:0.8,
+                duration: 0.8,
                 delay: 0.1,
-                stagger:0.1     ,
+                stagger: 0.05,
             }
         )
+
     })
     return (
-        <div class="overlay">
-            <div class="block block-1"></div>
-            <div class="block block-2"></div>
-            <div class="block block-3"></div>
-            <div class="block block-4"></div>
-            <div class="block block-5"></div>
-            <div class="block block-6"></div>
-            <div class="block block-7"></div>
-            <div class="block block-8"></div>
-            <div class="block block-9"></div>
-            <div class="block block-10"></div>
-            <div class="block block-11"></div>
-            <div class="block block-12"></div>
-            <div class="block block-13"></div>
-            <div class="block block-14"></div>
-            <div class="block block-15"></div>
-            <div class="block block-16"></div>
-            <div class="block block-17"></div>
-            <div class="block block-18"></div>
-            <div class="block block-19"></div>
-            <div class="block block-20"></div>
-            <div class="block block-21"></div>
-        </div>
+        <>
+            <div className="logo-anim absolute  w-screen h-screen flex justify-center items-center pointer-events-none">
+                <div className="translate-x-3 relative flex w-full h-full items-center justify-center">
+                    <Image
+                    src={'/assets/archiiveLoader.svg'}
+                    width={100}
+                    height={100}
+                    className="loader-logo"
+                    />
+                    <h1 className={`text-white ml-10 text-9xl loader-brand--text ${neueHass.className} font-medium`}>archiive</h1>
+                </div>
+            </div>
+            <div className="overlay pointer-events-none">
+                <div className="block block-1"></div>
+                <div className="block block-2"></div>
+                <div className="block block-3"></div>
+                <div className="block block-4"></div>
+                <div className="block block-5"></div>
+                <div className="block block-6"></div>
+                <div className="block block-7"></div>
+                <div className="block block-8"></div>
+                <div className="block block-9"></div>
+                <div className="block block-10"></div>
+                <div className="block block-11"></div>
+                <div className="block block-12"></div>
+                <div className="block block-13"></div>
+                <div className="block block-14"></div>
+                <div className="block block-15"></div>
+                <div className="block block-16"></div>
+                <div className="block block-17"></div>
+                <div className="block block-18"></div>
+                <div className="block block-19"></div>
+                <div className="block block-20"></div>
+                <div className="block block-21"></div>
+                <div className="block block-22"></div>
+            </div>
+        </>
     )
 }
 
--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,8 +1,10 @@
 import localFont from '@next/font/local';
 import { Monda } from 'next/font/google'
-
+import gsap, { Power1 } from 'gsap'
+import { useGSAP } from '@gsap/react'
+gsap.registerPlugin(useGSAP);
 const monda = Monda({ subsets: ['latin'], weight: "400" })
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 const neueHass = localFont({
     src: [
         {
@@ -19,13 +21,49 @@ const neueHass = localFont({
     ]
 });
 const Process = () => {
+    const processCard = useRef(null);
+    let horizontalScroll = 0;
+    useEffect(() => {
+        
+        if (processCard.current && window.innerWidth > 1300) {
+            const processCards = Array.from(processCard.current.childNodes)
+            processCards.slice(0,processCards.length-2).forEach((card) => {
+            
+                horizontalScroll += card.offsetWidth;
+            })
+            gsap.to("#process", {
+                scrollTrigger: {
+                    trigger: "#process",
+                    start: "top top",
+                    end: "+=2500",
+                    pin: true,
+                    markers: false,
+                    scrub: true
+                },
+                x: -(horizontalScroll),
+                ease: "power1.inOut",
+                delay: false
+            })
+        }
+    }, [])
+
+    useGSAP(() => {
+       
+        
+    
+    })
     return (
-        <div className='process-container mx-10 '>
+        <div className='process-container mx-10' id='process'>
             <div className="process-head">
                 <h2 className={`text-9xl ${neueHass.className} font-medium`}>Our Approach</h2>
             </div>
 
-            <div className="process-card--container h-full flex flex-row items-center">
+            <div className="process-card--container h-full flex flex-row items-center" ref={processCard}>
+                <div className="process-card flex flex-col place-content-between  border border-black rounded-lg p-10">
+                    <p className={`card-id ${monda.className}`}>(01)</p>
+                    <h2 className={`text-5xl ${neueHass.className} font-medium`}>Consultation and Discovery</h2>
+                    <p className={`${neueHass.className} text-2xl`}>At "archiive," our process starts with an in-depth consultation. We focus on understanding our clients' needs, preferences, and vision for their architectural photography projects. Through meaningful conversations, we ensure alignment between their goals and our expertise.</p>
+                </div>
                 <div className="process-card flex flex-col place-content-between  border border-black rounded-lg p-10">
                     <p className={`card-id ${monda.className}`}>(01)</p>
                     <h2 className={`text-5xl ${neueHass.className} font-medium`}>Consultation and Discovery</h2>
