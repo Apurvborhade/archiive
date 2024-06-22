e56f0e862ff0db527cd7c51165dabbdcae666120
Apurvborhade <apurvaborhadee@gmail.com>
2024-06-22 15:47:02 +0530

    landing page fix

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,14 +8,14 @@ const Landing = () => {
       <div className="brand-info flex justify-end">
         <h2 className={`${Helvetica_Bold.className} lg:w-3/6 text-4xl lg:text-5xl lg:mt-32 mt-44`}>We are a creative studio crafting unique visual content to empower people and brands.</h2>
       </div>
-      <div className={`branding ${FreeSans.className}`}>
+      <div className={`branding ${neueHass.className}`}>
         <div className="tagline-anim">
           <h1 className={`text-8xl lg:text-9xl ${neueHass.className} font-medium`}>archiive</h1>
           <div className="text-anim flex items-center text-lg lg:text-3xl">
-            <h2 className=''>We are here to archiive</h2>
-            <div className="dash lg:w-24 w-16 bg-black border border-black mx-5"></div>
+            <h2 className='inline w-3/6 leading-5  font-medium'>We are here to archiive</h2>
+            <div className="dash lg:w-24 w-10 bg-black border border-black mx-5"></div>
             <div className='text-slide h-6 lg:h-10 w-auto lg:w-auto overflow-hidden'>
-              <div className='text-transform'>
+              <div className='text-transform font-medium'>
                 <h2>Architecture</h2>
                 <h2>Landscape</h2>
                 <h2>Nature</h2>
